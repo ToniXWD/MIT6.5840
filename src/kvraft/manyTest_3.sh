7ab84fe1c4c2bd0ba133f29a0f493be9551391d4
ToniXWD <807077266@qq.com>
2024-01-30 02:38:11 +0800

    3A: Pass all tests in 3A 100 times

--- src/kvraft/manyTest_3.sh
+++ src/kvraft/manyTest_3.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # 初始化计数器
+target="$1"
 count=0
 success_count=0
 fail_count=0
 
 # 设置测试次数
-max_tests=50
+max_tests=100
 
 for ((i=1; i<=max_tests; i++))
 do
     echo "Running test iteration $i of $max_tests..."
     
     # 运行 go 测试命令
-    go test -v -run TestFigure8Unreliable2C &> output2C_index.log
+    go test -v -run "$target" &> output"$target".log
     
     # 检查 go 命令的退出状态
     if [ "$?" -eq 0 ]; then
@@ -21,12 +22,12 @@ do
         success_count=$((success_count+1))
         echo "Test iteration $i passed."
         # 如果想保存通过的测试日志，取消下面行的注释
-        # mv output2C_index.log "success_$i.log"
+        # mv output"target$".log "success_$i.log"
     else
         # 测试失败
         fail_count=$((fail_count+1))
-        echo "Test iteration $i failed, check 'failure2C_$i.log' for details."
-        mv output2C_index.log "failure2C_$i.log"
+        echo "Test iteration $i failed, check 'failure"$target"_$i.log' for details."
+        mv output"target$".log "failure"$target"_$i.log"
     fi
 done
 
